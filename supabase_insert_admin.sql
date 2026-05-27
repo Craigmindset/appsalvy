78180097acecad929232c7524ea973c31578617e
craigmindset <irediacraig@gmail.com>
2026-05-28 00:24:51 +0100

    updated

--- supabase_insert_admin.sql
+++ supabase_insert_admin.sql
@@ -1,10 +1,10 @@
 -- Insert admin user with hashed password
--- Email: admin@salvyventure.com
--- Password: admin123456
--- SHA-256 hash of 'admin123456': 240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9
+-- Email: salvyweb1@gmail.com
+-- Password: Salvyweb1#!57
+-- SHA-256 hash of 'Salvyweb1#!57': abbb1424c66c142e48d865eb3d3694b3ee72db8db813e5505eba02b3531999e3
 
 INSERT INTO public.admin (email, password, name, role)
-VALUES ('salvyweb1@gmail.com', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', 'Admin User', 'admin')
+VALUES ('salvyweb1@gmail.com', 'abbb1424c66c142e48d865eb3d3694b3ee72db8db813e5505eba02b3531999e3', 'Admin User', 'admin')
 ON CONFLICT (email) 
 DO UPDATE SET 
   password = EXCLUDED.password,
